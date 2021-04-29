679b148c56275ad14e539747e332a2520319fb1c
handsome <3393402356@qq.com>
2021-04-29 13:18:13 +0800

    完成了权限列表

--- src/views/rights/Rights.vue
+++ src/views/rights/Rights.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="">
-    <h1>rights</h1>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>权限列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片显示权限列表 -->
+    <el-card>
+      <el-table border :data="rightList" style="width: 100%">
+        <el-table-column type="index" label="#"></el-table-column>
+        <el-table-column prop="authName" label="权限名称"></el-table-column>
+        <el-table-column prop="path" label="路径"></el-table-column>
+        <el-table-column label="权限等级">
+          <template #default="{ row }">
+            <el-tag v-if="row.level == 0">一级</el-tag>
+            <el-tag type="success" v-else-if="row.level == 1">二级</el-tag>
+            <el-tag type="warning" v-else>三级</el-tag>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
 <script>
+import { reqRightAll } from "../../network/api";
 export default {
-  name: '',
-  data() { 
+  name: "",
+  data() {
     return {
-
-    }
+      rightList: [],
+    };
   },
-  methods:{},
- }
+  methods: {
+    async getRightAll() {
+      const { meta, data } = await reqRightAll("list");
+      if (meta.status !== 200) return this.$message.error(meta.msg);
+      this.rightList = data;
+    },
+  },
+  created() {
+    this.getRightAll();
+  },
+};
 </script>
 
 <style lang="less" scoped>
